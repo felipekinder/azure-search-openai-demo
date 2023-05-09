381f8c0563a1e7891d63b4a3c032eebf41dd7a42
felipekinder <132633089+felipekinder@users.noreply.github.com>
2023-05-09 16:14:51 +0000

    ajustes

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -12,7 +12,7 @@ const EXAMPLES: ExampleModel[] = [
         text: "Qual a missão da empresa?",
         value: "Qual a missão da E-Create?"
     },
-    { text: "Qual o plano de saúde da empresa?", value: "Qual o plano de saúde da E-Create?" },
+    { text: "Qual o plano de saúde oferecido pela empresa?", value: "Qual o plano de saúde oferecido pela E-Create?" },
     {
         text: " A empresa tem normas e procedimentos para a conduta dos funcionários no ambiente de trabalho?",
         value: "A empresa tem normas e procedimentos para a conduta dos funcionários no ambiente de trabalho?"
